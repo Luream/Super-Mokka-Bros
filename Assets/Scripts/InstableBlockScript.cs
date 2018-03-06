393fa6a4a60e7fd50dd60bb05ff6b789d92afd8d
RexFabianus <36882225+RexFabianus@users.noreply.github.com>
2018-03-06 13:29:41 +0100

    Add files via upload
    Added: InstableBlockScript: Block verschwindet, nachdem der Player diesen verlässt. Ist instantDestroy true, wird der Block bei dem Betreten zerstört

---
+++ Assets/Scripts/InstableBlockScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstableBlockScript : MonoBehaviour {
+
+
+    public Boolean instantDestroy = false;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && instantDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
