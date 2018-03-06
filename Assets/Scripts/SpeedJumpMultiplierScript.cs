b9d219462ae62b5ac54850f6bb6796a48ab6253b
RexFabianus <36882225+RexFabianus@users.noreply.github.com>
2018-03-06 11:47:33 +0100

    Add files via upload

--- Assets/Scripts/SpeedJumpMultiplierScript.cs
+++ Assets/Scripts/SpeedJumpMultiplierScript.cs
@@ -7,11 +7,30 @@ public class SpeedJumpMultiplierScript : MonoBehaviour {
     public float speedMultiplier = 2;
     public float JumpMultiplier = 2;
 
+    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        GameObject player = GameObject.Find("Player");
+
         if (collision.gameObject.CompareTag("Player"))
         {
-
+            player.GetComponent<PlayerScript>().movementSpeed *= speedMultiplier;
+            player.GetComponent<PlayerScript>().jumpSpeed *= JumpMultiplier;
         }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        GameObject player = GameObject.Find("Player");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerScript>().movementSpeed /= speedMultiplier;
+            player.GetComponent<PlayerScript>().jumpSpeed /= JumpMultiplier;
+        }
+    }
+
 }
