b22f2e15ef035795c6ed04074aa97d5dd339d119
RexFabianus <36882225+RexFabianus@users.noreply.github.com>
2018-03-10 22:40:11 +0100

    Add files via upload

--- Assets/Scripts/InstableBlockScript.cs
+++ Assets/Scripts/InstableBlockScript.cs
@@ -7,10 +7,13 @@ public class InstableBlockScript : MonoBehaviour {
 
 
     public Boolean instantDestroy = false;
+    public Boolean onlyTriggeredFromAbove = true;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player") && instantDestroy)
+        
+        
+        if(collision.gameObject.CompareTag("Player") && instantDestroy && trigger())
         {
             Destroy(gameObject);
         }
@@ -18,9 +21,30 @@ public class InstableBlockScript : MonoBehaviour {
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && trigger())
         {
             Destroy(gameObject);
+        }
+    }
+
+    private Boolean trigger()
+    {
+        if (onlyTriggeredFromAbove)
+         {
+            GameObject player = GameObject.Find("Player");
+            float ykoordinatePlayerUnten = player.transform.position.y - 0.5f * player.transform.localScale.y;
+            float ykoordinatePlatformOben = transform.position.y + 0.5f * transform.localScale.y;
+
+            if (ykoordinatePlayerUnten  < ykoordinatePlatformOben)
+              {
+                 return false;
+              } else
+              {
+                 return true;
+              }
+        } else
+        {
+            return true;
         }
     }
 }
